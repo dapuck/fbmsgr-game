1888b71a627455c051ad26932eabb8ca36cd578d
dapuck <imccall@da-puck.com>
2016-04-26 07:33:29 -0500

    Add function to get profile. Refactor.

--- messenger.js
+++ messenger.js
@@ -5,7 +5,7 @@ const qs = require("querystring");
 
 class FBMessenger {
     constructor(token) {
-        this._fbBase = "https://graph.facebook.com/v2.6/me/";
+        this._fbBase = "https://graph.facebook.com/v2.6/";
         this._token = token;
         this._q = qs.stringify({access_token: token});
         this._wreck = Wreck.defaults({
@@ -19,13 +19,34 @@ class FBMessenger {
             recipient: { id: userid },
             message: msg
         });
-        return new Promise(function(resolve, reject) {
+        return this._makeRequest(`/me/messages?${this._q}`,payload);
+    }
+    
+    getUserProfile(userid, fields) {
+        let params = {
+            access_token: this._token
+        };
+        let q = "";
+        fields = (Array.isArray(fields)) ? fields.join(",") : fields;
+        if(fields) {
+            params.fields = fields;
+        }
+        q = qs.stringify(params);
+        return this._makeRequest(`/${userid}?${q}`);
+    }
+    
+    _makeRequest(url, payload) {
+        let method = "GET";
+        let opts = {};
+        if(payload) {
+            method = "POST";
+            opts.payload = payload;
+        }
+        return new Promise((resolve, reject) => {
             this._wreck.request(
-                "POST",
-                `/messages?${this._q}`,
-                {
-                    payload: payload
-                },
+                method,
+                url,
+                opts,
                 (err, response) => {
                     if(err) {
                         return reject(err);
